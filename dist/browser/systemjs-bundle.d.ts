ab5693158e6cfa514138e2fc8f224c9be767816d
ttristan <green.00rumor@icloud.com>
2022-09-17 14:59:23 +0200

    ADD types

---
+++ dist/browser/systemjs-bundle.d.ts
@@ -0,0 +1,16 @@
+declare module "lib" {
+    export const removeAllScrollLocks: () => void;
+    export const removeScrollLock: (element: HTMLElement) => void;
+    export const lockBodyScroll: () => void;
+    export const lockElement: (element: HTMLElement) => void;
+    export const registerLockIdOnBody: (id: string) => void;
+    export const registerLockIdOnElement: (element: HTMLElement, id: string) => string;
+}
+declare module "index" {
+    import { lockBodyScroll, removeAllScrollLocks, removeScrollLock } from "lib";
+    export { removeScrollLock, useBodyScrollLock, lockBodyScroll, removeAllScrollLocks, };
+    export default function useBodyScrollLock(id: string, containerElement: HTMLElement, scrollContentElement?: HTMLElement): {
+        removeScrollLock: () => void;
+        removeAllScrollLocks: () => void;
+    };
+}
---
+++ dist/lib.d.ts
@@ -0,0 +1,6 @@
+export declare const removeAllScrollLocks: () => void;
+export declare const removeScrollLock: (element: HTMLElement) => void;
+export declare const lockBodyScroll: () => void;
+export declare const lockElement: (element: HTMLElement) => void;
+export declare const registerLockIdOnBody: (id: string) => void;
+export declare const registerLockIdOnElement: (element: HTMLElement, id: string) => string;
