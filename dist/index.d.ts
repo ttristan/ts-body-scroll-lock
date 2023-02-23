035f6e271a223efe9561f9addc3afeebb9b71de3
Miraya Abrodi <miraya.abrodi@check24.de>
2023-02-23 14:32:34 +0100

    run build

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { lockBodyScroll, removeAllScrollLocks, removeScrollLock } from "./lib";
-export { removeScrollLock, useBodyScrollLock, lockBodyScroll, removeAllScrollLocks, };
-export default function useBodyScrollLock(id: string, containerElement: HTMLElement, scrollContentElement?: HTMLElement): {
-    removeScrollLock: () => void;
-    removeAllScrollLocks: () => void;
-};
